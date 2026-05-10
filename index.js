9917549adf760d05199c186bbed7f31172730b16
ailaigouchuan-netizen <ailaigouchuan@gmail.com>
2026-05-10 18:37:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("bot start");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log("bot start");
+});
+
+client.login(process.env.DISCORD_TOKEN);
