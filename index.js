5314a87cae6b5ad8e28d7974c42cd9b359f154fa
ailaigouchuan-netizen <ailaigouchuan@gmail.com>
2026-05-11 00:34:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
 const { Client, GatewayIntentBits } = require('discord.js');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(3000, () => {
+  console.log('Web server started');
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
