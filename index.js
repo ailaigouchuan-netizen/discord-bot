9280aee0f7795d4a86907907053fe33db9d5b556
ailaigouchuan-netizen <ailaigouchuan@gmail.com>
2026-05-11 18:48:14 +0900

    Remove Express server and add message reply feature
    Removed Express server setup and added message handling for 'こんにちは'.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require('express');
 const { Client, GatewayIntentBits } = require('discord.js');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bot is running');
-});
-
-// Render用ポート（これ重要）
-app.listen(process.env.PORT || 3000);
 
 const client = new Client({
   intents: [
@@ -21,5 +11,10 @@ client.once('ready', () => {
   console.log('Bot online!');
 });
 
+client.on('messageCreate', message => {
+  if (message.content === 'こんにちは') {
+    message.reply('こんにちは！');
+  }
+});
+
 client.login(process.env.DISCORD_TOKEN);
- 
